86bd855d21e5c43c7b9ec2c6a10d63f719b0ee11
Petter Sæther Moen <pettersmoen@gmail.com>
2024-07-08 12:04:36 +0200

    add drone certificate to /drone

--- next.config.js
+++ next.config.js
@@ -3,7 +3,14 @@ import withMDX from '@next/mdx'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['tsx', 'mdx'],
-
+  rewrites: async () => {
+    return [
+      {
+        source: '/drone',
+        destination: '/drone/UserCertificate.pdf',
+      },
+    ];
+  },
 };
 
 export default withMDX()(nextConfig);
