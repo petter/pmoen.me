ac0b392b4d7f57ebf10c0124eb58d74ecdc8881e
Petter Sæther Moen <pettersmoen@gmail.com>
2025-01-24 15:50:38 +0100

    add support for code blocks

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -2,6 +2,7 @@ import type { MDXComponents } from 'mdx/types';
 import { Heading } from './components/typography/heading';
 import { InlineCode } from './components/typography/inline-code';
 import { Link } from './components/typography/link';
+import { CodeBlock } from './components/code-block';
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
@@ -11,7 +12,17 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
     h4: (props) => <Heading level={4} {...props} />,
     h5: (props) => <Heading level={5} {...props} />,
     h6: (props) => <Heading level={6} {...props} />,
-    code: (props) => <InlineCode {...props} />,
+    code: (props) => {
+      if (props.className) {
+        return (
+          <CodeBlock
+            language={props.className.split('language-')[1]}
+            code={props.children}
+          />
+        );
+      }
+      return <InlineCode {...props} />;
+    },
     a: (props) => <Link href={props.href ?? ''} {...props} />,
     ...components,
   };
