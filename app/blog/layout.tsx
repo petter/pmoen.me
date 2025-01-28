4fff3c6109ecc5f7902872b2bf3381b6a3c12688
Petter Sæther Moen <pettersmoen@gmail.com>
2025-01-28 21:16:16 +0100

    add some more spacing between parts of blog posts

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -3,5 +3,9 @@ import type { PropsWithChildren } from 'react';
 type CenteredLayoutProps = PropsWithChildren<never>;
 
 export default function CenteredLayout({ children }: CenteredLayoutProps) {
-  return <div className="mx-auto w-full max-w-4xl px-4 py-8">{children}</div>;
+  return (
+    <div className="mx-auto flex w-full max-w-4xl flex-col gap-4 px-4 py-8">
+      {children}
+    </div>
+  );
 }
