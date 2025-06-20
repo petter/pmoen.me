1febe4bf3f9365040343cd19a961bbe4c0c811a0
Petter Sæther Moen <pettersmoen@gmail.com>
2025-06-20 09:49:08 +0200

    migrate to tailwind v4

--- app/_footer/footer.tsx
+++ app/_footer/footer.tsx
@@ -2,7 +2,7 @@ import { Socials } from './socials';
 
 export async function Footer() {
   return (
-    <footer className="min-h-[var(--min-footer-height)] w-full bg-stone-950 p-8 text-stone-100">
+    <footer className="min-h-(--min-footer-height) w-full bg-stone-950 p-8 text-stone-100">
       <Socials />
     </footer>
   );
