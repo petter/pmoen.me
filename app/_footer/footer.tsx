f0c2854cbf407f48c18b1019135b86f651d59969
Petter Sæther Moen <pettersmoen@gmail.com>
2025-11-28 17:08:26 +0100

    remove unused components

--- app/_footer/footer.tsx
+++
@@ -1,9 +0,0 @@
-import { Socials } from './socials';
-
-export async function Footer() {
-  return (
-    <footer className="min-h-(--min-footer-height) w-full bg-stone-950 p-8 text-stone-100">
-      <Socials />
-    </footer>
-  );
-}
